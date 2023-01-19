9c7dabdd5d4c3701cd990771e82225403661c112
Brett Smith <67445684+brettsmith212@users.noreply.github.com>
2023-01-19 05:20:37 +0000

    remove console logs

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -3,7 +3,5 @@ import { clientEnv } from "../env/schema.mjs";
 
 const supabaseUrl = clientEnv.NEXT_PUBLIC_SUPABASE_URL || "TEST"
 const supabaseAnonKey = clientEnv.NEXT_PUBLIC_SUPABASE_ANON_KEY || "TEST"
-console.log("SUPABASE URL: ", supabaseUrl)
-console.log("SUPABASE ANON: ", supabaseAnonKey)
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
